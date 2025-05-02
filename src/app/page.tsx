1caaf9b7ea508c9960b1117313861ddd75a9f5cb
ulugbekAbdullayev <ulu23503@gmail.com>
2025-05-03 03:55:26 +0500

    respons+

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Header from "@/components/Header";
+import Loyha from "@/components/Loyha";
+import Main from "@/components/Main";
+
 
 export default function Home() {
   return (
     <div >
-     
+     <Header/>
+        <Main/>
+        <Loyha/>
     </div>
   );
 }
